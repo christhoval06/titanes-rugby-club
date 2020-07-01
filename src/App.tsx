1e9bc183d8ca8c983decf77741c8c4eb6ced9f96
Christhoval Barba Rivas <christhoval06@gmail.com>
2020-07-01 15:21:38 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Titanes Rugby Club</h1>
+      <h2>Hemandad, Honor y Disciplina</h2>
+    </div>
+  );
+}
